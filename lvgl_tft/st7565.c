918d4c2baa16fb8772b170ee6ffd4b6d15a7df01
Matt Staniszewski <matt@msxconsulting.com>
2023-05-18 10:56:27 -0700

    Hook up ST7565 driver into our driver branch

---
+++ lvgl_tft/st7565.c
@@ -0,0 +1,229 @@
+/**
+ * @file st7565.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "st7565.h"
+#include "disp_spi.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+ #define TAG "ST7565"
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/*The LCD needs a bunch of command/argument values to be initialized. They are stored in this struct. */
+typedef struct {
+    uint8_t cmd;
+    uint8_t data[16];
+    uint8_t databytes; //No of data in data; bit 7 = delay after set; 0xFF = end of cmds.
+} lcd_init_cmd_t;
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void st7565_send_cmd(uint8_t cmd);
+static void st7565_send_data(void * data, uint16_t length);
+static void st7565_sync(int32_t x1, int32_t y1, int32_t x2, int32_t y2);
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+static uint8_t lcd_fb[ST7565_HOR_RES * ST7565_VER_RES / 8] = {0xAA, 0xAA};
+static uint8_t pagemap[] = { 3, 2, 1, 0, 7, 6, 5, 4 };  //{ 7, 6, 5, 4, 3, 2, 1, 0 };
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+void st7565_init(void)
+{
+    // Set up LCD initialization structure
+    lcd_init_cmd_t init_cmds[]={
+		{ST7565_SET_BIAS_7, {0}, 0x00},                 // LCD bias select
+		{ST7565_SET_SEG_NORMAL, {0}, 0x00},             // SEG select
+		{ST7565_SET_COM_NORMAL, {0}, 0x00},             // COM select
+		{ST7565_SET_DISP_START_LINE | 0x20, {0}, 0x00}, // Initial display line, screen starts halfway down
+#if ST7565_INVERT_COLORS == 1
+		{ST7565_SET_DISP_INVERSE, {0}, 0x00},           // Set inverted mode
+#else
+ 		{ST7565_SET_DISP_NORMAL, {0}, 0x00},            // Set non-inverted mode
+#endif
+		{ST7565_SET_POWER_CONTROL | 0x04, {0}, 0x80},   // Turn on voltage converter (VC=1, VR=0, VF=0)     
+		{ST7565_SET_POWER_CONTROL | 0x06, {0}, 0x80},   // Turn on voltage regulator (VC=1, VR=1, VF=0)
+		{ST7565_SET_POWER_CONTROL | 0x07, {0}, 0x80},   // Turn on voltage follower (VC=1, VR=1, VF=1)
+#if ST7565_INVERT_COLORS == 1
+		{ST7565_SET_RESISTOR_RATIO | 0x05, {0}, 0x00},  // Set lcd operating voltage (regulator resistor, ref voltage resistor); RR = 5.5
+		{ST7565_SET_VOLUME_FIRST, {0}, 0x00},     
+		{ST7565_SET_VOLUME_SECOND | 0x17, {0}, 0x00},   // Set the contrast; EV = 23 = 0x17
+#else
+		{ST7565_SET_RESISTOR_RATIO | 0x06, {0}, 0x00},  // Set lcd operating voltage (regulator resistor, ref voltage resistor); RR = 6.0
+		{ST7565_SET_VOLUME_FIRST, {0}, 0x00},     
+		{ST7565_SET_VOLUME_SECOND | 0x18, {0}, 0x00},   // Set the contrast; EV = 24 = 0x18
+#endif
+		{ST7565_DISPLAY_ON, {0x00}, 0x00},              // Turn on display   
+		{ST7565_SET_ALLPTS_NORMAL, {0}, 0x00},          // Reset all pixels  
+		{0, {0}, 0xff}
+    };
+
+    // Apply the backlight LEDC PWM timer/channel configuration, turn backlight on
+    ledc_timer_config_t disp_bl_timer = {
+        .speed_mode       = DISP_BL_LEDC_MODE,
+        .timer_num        = DISP_BL_LEDC_TIMER,
+        .duty_resolution  = DISP_BL_LEDC_DUTY_RES,
+        .freq_hz          = DISP_BL_LEDC_FREQUENCY,
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&disp_bl_timer));
+
+    ledc_channel_config_t disp_bl_channel = {
+        .speed_mode     = DISP_BL_LEDC_MODE,
+        .channel        = DISP_BL_LEDC_CHANNEL,
+        .timer_sel      = DISP_BL_LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = DISP_BL_PWM_PIN,
+        .duty           = DUTY_TO_LEDC_VALUE(100), // TEMP: Make it 100%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&disp_bl_channel));
+
+	// Initialize non-SPI GPIOs
+    gpio_reset_pin(ST7565_DC);
+	gpio_set_direction(ST7565_DC, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(ST7565_RST);
+	gpio_set_direction(ST7565_RST, GPIO_MODE_OUTPUT);
+
+	// Reset the display
+	gpio_set_level(ST7565_RST, 0);
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+	gpio_set_level(ST7565_RST, 1);
+	vTaskDelay(100 / portTICK_PERIOD_MS);
+
+	ESP_LOGI(TAG, "ST7565 initialization.");
+
+	// Send all the commands
+	uint16_t cmd = 0;
+	while (init_cmds[cmd].databytes != 0xff) {
+
+		st7565_send_cmd(init_cmds[cmd].cmd);
+
+        // Send data if present
+        if (init_cmds[cmd].data) {
+		    st7565_send_data(init_cmds[cmd].data, init_cmds[cmd].databytes & 0x1F);
+        }
+		
+        // Delay if requested
+        if (init_cmds[cmd].databytes & 0x80) {
+			vTaskDelay(100 / portTICK_PERIOD_MS);
+		}
+		
+        cmd++;
+	}
+
+    // Clear the local buffer
+    memset(lcd_fb, 0x00, sizeof(lcd_fb));
+}
+
+void st7565_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map)
+{
+    // Return if the area is out the screen
+    if (area->x2 < 0) return;
+    if (area->y2 < 0) return;
+    if (area->x1 > ST7565_HOR_RES - 1) return;
+    if (area->y1 > ST7565_VER_RES - 1) return;
+
+    // Truncate the area to the screen
+    int32_t act_x1 = area->x1 < 0 ? 0 : area->x1;
+    int32_t act_y1 = area->y1 < 0 ? 0 : area->y1;
+    int32_t act_x2 = area->x2 > ST7565_HOR_RES - 1 ? ST7565_HOR_RES - 1 : area->x2;
+    int32_t act_y2 = area->y2 > ST7565_VER_RES - 1 ? ST7565_VER_RES - 1 : area->y2;
+
+    int32_t x, y;
+
+    // Refresh frame buffer
+    for(y = act_y1; y <= act_y2; y++) {
+        for(x = act_x1; x <= act_x2; x++) {
+            if (lv_color_to1(*color_map) != 0) {
+                lcd_fb[x + (y / 8)*ST7565_HOR_RES] &= ~(1 << (7 - (y % 8)));
+            } else {
+                lcd_fb[x + (y / 8)*ST7565_HOR_RES] |= (1 << (7 - (y % 8)));
+            }
+            color_map++;
+        }
+        color_map += area->x2 - act_x2; // Next row
+    }
+    st7565_sync(act_x1, act_y1, act_x2, act_y2);
+}
+
+void st7565_enable_backlight(bool backlight) {
+
+    uint32_t pwm_duty = (backlight ? 100 : 0);  // TEMP: Set to 0% or 100%
+
+    // Set duty to given value
+    ESP_ERROR_CHECK(ledc_set_duty(DISP_BL_LEDC_MODE, DISP_BL_LEDC_CHANNEL, DUTY_TO_LEDC_VALUE(pwm_duty)));
+
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(DISP_BL_LEDC_MODE, DISP_BL_LEDC_CHANNEL));
+}
+
+void st7565_sleep_in()
+{
+	st7565_send_cmd(ST7565_DISPLAY_OFF);
+	st7565_send_cmd(ST7565_SET_ALLPTS_ON);
+}
+
+void st7565_sleep_out()
+{
+    st7565_send_cmd(ST7565_SET_ALLPTS_NORMAL);
+	st7565_send_cmd(ST7565_DISPLAY_ON);
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+static void st7565_send_cmd(uint8_t cmd)
+{
+	disp_wait_for_pending_transactions();
+	gpio_set_level(ST7565_DC, 0);   // Command mode
+	disp_spi_send_data(&cmd, 1);
+}
+
+static void st7565_send_data(void * data, uint16_t length)
+{
+	disp_wait_for_pending_transactions();
+	gpio_set_level(ST7565_DC, 1);   // Data mode
+	disp_spi_send_data(data, length);
+}
+
+static void st7565_sync(int32_t x1, int32_t y1, int32_t x2, int32_t y2)
+{
+    uint8_t c, p;
+
+    for(p = y1 / 8; p <= y2 / 8; p++) {
+
+        st7565_send_cmd(ST7565_SET_PAGE | pagemap[p]);
+        st7565_send_cmd(ST7565_SET_COLUMN_LOWER | ((x1 + DISP_COL_OFFSET) & 0xf));
+        st7565_send_cmd(ST7565_SET_COLUMN_UPPER | (((x1 + DISP_COL_OFFSET) >> 4) & 0xf));
+        st7565_send_cmd(ST7565_RMW);
+
+        for(c = x1; c <= x2; c++) {
+            st7565_send_data(&lcd_fb[(ST7565_HOR_RES * p) + c], 1);
+        }
+    }
+}
---
+++ lvgl_tft/st7565.h
@@ -0,0 +1,108 @@
+/**
+ * @file lv_templ.h
+ *
+ */
+
+#ifndef ST7565_H
+#define ST7565_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdbool.h>
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl/lvgl.h"
+#endif
+
+/*********************
+ *      DEFINES
+ *********************/
+
+#define ST7565_DC   CONFIG_LV_DISP_PIN_DC
+#define ST7565_RST  CONFIG_LV_DISP_PIN_RST
+
+#define ST7565_HOR_RES  128 //CONFIG_LV_HOR_RES_MAX - not defined in LVGL v8
+#define ST7565_VER_RES  64  //CONFIG_LV_VER_RES_MAX - not defined in LVGL v8
+
+#define ST7565_INVERT_COLORS CONFIG_LV_INVERT_COLORS
+
+// ST7565 specific commands used in init
+#define ST7565_DISPLAY_OFF            0xAE
+#define ST7565_DISPLAY_ON             0xAF
+
+#define ST7565_SET_DISP_START_LINE    0x40
+#define ST7565_SET_PAGE               0xB0
+
+#define ST7565_SET_COLUMN_UPPER  	  0x10
+#define ST7565_SET_COLUMN_LOWER  	  0x00
+
+#define ST7565_SET_SEG_NORMAL         0xA0
+#define ST7565_SET_SEG_REVERSE        0xA1
+
+#define ST7565_SET_DISP_NORMAL        0xA6
+#define ST7565_SET_DISP_INVERSE       0xA7
+
+#define ST7565_SET_ALLPTS_NORMAL      0xA4
+#define ST7565_SET_ALLPTS_ON          0xA5
+#define ST7565_SET_BIAS_9             0xA2 
+#define ST7565_SET_BIAS_7             0xA3
+
+#define ST7565_RMW                    0xE0
+#define ST7565_RMW_CLEAR              0xEE
+#define ST7565_INTERNAL_RESET         0xE2
+#define ST7565_SET_COM_NORMAL         0xC0
+#define ST7565_SET_COM_REVERSE        0xC8
+#define ST7565_SET_POWER_CONTROL      0x28
+#define ST7565_SET_RESISTOR_RATIO     0x20
+#define ST7565_SET_VOLUME_FIRST       0x81
+#define ST7565_SET_VOLUME_SECOND      0
+#define ST7565_SET_BOOSTER_FIRST      0xF8
+#define ST7565_SET_BOOSTER_234        0
+#define ST7565_SET_BOOSTER_5          1
+#define ST7565_SET_BOOSTER_6          3
+#define ST7565_NOP                    0xE3
+#define ST7565_TEST                   0xF0
+
+//TEMP
+#define DISP_COL_OFFSET                 0x3 // Shift start of columns by this amount
+
+#define DISP_BL_PWM_PIN     34
+
+#define DISP_BL_LEDC_TIMER              LEDC_TIMER_0
+#define DISP_BL_LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define DISP_BL_LEDC_CHANNEL            LEDC_CHANNEL_0
+#define DISP_BL_LEDC_DUTY_RES           LEDC_TIMER_11_BIT // Set duty resolution to 11 bits
+#define DISP_BL_LEDC_FREQUENCY          25000   // Frequency in Hertz. Set frequency at 25 kHz
+
+#define DUTY_TO_LEDC_VALUE(x)           (((2048 - 1) * x) / 100) // Converts duty cycle to LEDC duty value
+//TEMP
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void st7565_init(void);
+void st7565_flush(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t * color_map);
+void st7565_enable_backlight(bool backlight);
+void st7565_sleep_in(void);
+void st7565_sleep_out(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*ST7565_H*/
